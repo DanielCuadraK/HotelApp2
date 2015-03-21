c3351e864b68a5af5d51896ea75b437cd8155ee1
dck26 <daniel.cuadra@gmail.com>
2015-03-21 13:09:10 -0600

    4

--- js/API/connection.js
+++ js/API/connection.js
@@ -1,5 +1,6 @@
 var connection = {
     isConnected: function(){
+        alert(navigator.connection.type);
         if(navigator.connection.type != Connection.NONE)
             return true;
         else
